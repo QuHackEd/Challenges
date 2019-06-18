621faeb19fa7664cfcc4a461947348f24da2b78c
BrianCoyle <s1671233@sms.ed.ac.uk>
2019-06-18 10:08:35 +0100

    Measurement challenge started

--- tests/test_challenge_measurement.py
+++ tests/test_challenge_measurement.py
@@ -1,10 +1,36 @@
-from challenges.challenge1_measurement import measurement_1, measurement_2
 from pyquil.api import WavefunctionSimulator, local_qvm
 import math
 import numpy as np
 
-def test_meas_outcome(results):
-    print('fuck ya')
-    if type(results) is not np.ndarray:
-        print("Type Error: Did you measure in the correct way?")
-    else: print('fuck ya')
+def test_measurement_run_results(results, num_shots):
+    
+    if not isinstance(results, np.ndarray):
+        print('It doesn\'t look like the results are in the correct format.. did you use run?')
+        
+    else:
+        results_shape = results.shape
+        if not results_shape[1] == 2:
+            print('It looks like the results array is the wrong size.')
+            print('Did you measure more than two qubits??')
+        else:
+            try:
+                   summed_results = np.sum(results, 0)
+            except:
+                  print('Something went wrong')
+            if abs(summed_results[0] - num_shots/2) < 0.1*num_shots:
+                print("Those statistics for the 3rd qubit look right, well done!")
+
+                if abs(summed_results[1] - num_shots/2) < 0.1*num_shots:
+                    print("The statistics for the 4th qubit look good as well, awesome!")
+                else:
+                    print("oops, those statistics don't look right... It could just be that you got a disproportionately high number of one outcome or another though. Try a couple more times.")
+
+                    if num_shots < 50:
+                        print('Maybe try a bigger number of  measurement shots..')
+
+            else:
+                print("oops, those statistics don't look right... It could just be that you got a disproportionately high number of one outcome or another though. Try a couple more times.")
+
+                if num_shots < 50:
+                     print('Maybe try a bigger number of  measurement shots..')
+                 
--- tests/test_single_qubit_unitaries.py
+++ tests/test_single_qubit_unitaries.py
@@ -6,86 +6,93 @@ def test_bit_flip_unitary(prog):
     wf_sim = WavefunctionSimulator()
 
     with local_qvm():
-        
-        state = wf_sim.wavefunction(prog)
+        if len(prog._instructions) == 0:
+            print("Hmm, I'm not sure you have any gates applied to the state. Better add some..") 
+        else:    
+            state = wf_sim.wavefunction(prog)
 
-        amp = 1
-
-        if abs(state[1].real - amp) < 0.0001:
-            print("\nCongratulations, you built the state:")
-            print(state)
-        else:
-            print("oops, you built the state:")
-            print(state)
-            print("Perhaps an alternative unitary would work?\n")
+            amp = 1
+            if abs(state[1].real - amp) < 0.0001:
+                print("\nCongratulations, you built the state:")
+                print(state)
+            else:
+                print("oops, you built the state:")
+                print(state)
+                print("Perhaps an alternative unitary would work?\n")
 
 def test_superposition_unitary(prog):
     wf_sim = WavefunctionSimulator()
    
     with local_qvm():
-        
-        state = wf_sim.wavefunction(prog)
+        if len(prog._instructions) == 0:
+            print("Hmm, I'm not sure you have any gates applied to the state. Better add some..") 
+        else:    
+            state = wf_sim.wavefunction(prog)
 
-        amp_plus    = 1 / math.sqrt(2)
-        amp_minus   = - 1 / math.sqrt(2)
-        if abs(state[0].real - amp_plus) < 0.0001 and abs(state[1].real - amp_plus) < 0.0001:
-            print("\nGood job! You built the superposition state:")
-            print(state)
+            amp_plus    = 1 / math.sqrt(2)
+            amp_minus   = - 1 / math.sqrt(2)
+            if abs(state[0].real - amp_plus) < 0.0001 and abs(state[1].real - amp_plus) < 0.0001:
+                print("\nGood job! You built the superposition state:")
+                print(state)
 
-        elif abs(state[0].real - amp_plus) < 0.0001 and abs(state[1].real - amp_minus) < 0.0001:
-            print("\nNot quite :(, you built a different superposition state:")
-            print(state)
+            elif abs(state[0].real - amp_plus) < 0.0001 and abs(state[1].real - amp_minus) < 0.0001:
+                print("\nNot quite :(, you built a different superposition state:")
+                print(state)
 
-        elif abs(state[0].real - amp_minus) < 0.0001 and abs(state[1].real - amp_plus) < 0.0001:
-            print("\nNot quite :(, you built a different superposition state:")
-            print(state)
+            elif abs(state[0].real - amp_minus) < 0.0001 and abs(state[1].real - amp_plus) < 0.0001:
+                print("\nNot quite :(, you built a different superposition state:")
+                print(state)
 
-        elif abs(state[0].real - amp_minus) < 0.0001 and abs(state[1].real - amp_minus) < 0.0001:
-            print("\nNot quite :(, you built a different superposition state:")
-            print(state)
-            print("This state is actually physically indistinguisable from the correct state up to a global phase. However, it's still not quite right.")
+            elif abs(state[0].real - amp_minus) < 0.0001 and abs(state[1].real - amp_minus) < 0.0001:
+                print("\nNot quite :(, you built a different superposition state:")
+                print(state)
+                print("This state is actually physically indistinguisable from the correct state up to a global phase. However, it's still not quite right.")
 
-        else:
-            print("\nHmm, you built the state:")
-            print(state)
-            print("Maybe try a different gate?\n")
+            else:
+                print("\nHmm, you built the state:")
+                print(state)
+                print("Maybe try a different gate?\n")
 
 def test_plus_input_to_zero(prog):
 
     wf_sim = WavefunctionSimulator()
 
     with local_qvm():
-        
-        state = wf_sim.wavefunction(prog)
+        if len(prog._instructions) == 0:
+            print("Hmm, I'm not sure you have any gates applied to the state. Better add some..") 
+        else:    
+            state = wf_sim.wavefunction(prog)
 
-        amp = 1
+            amp = 1
 
-        if abs(state[0].real - amp) < 0.0001:
-            print("\nExcellent! You built the state:")
-            print(state)
+            if abs(state[0].real - amp) < 0.0001:
+                print("\nExcellent! You built the state:")
+                print(state)
 
-        else:
-            print("oops, you built the state:")
-            print(state)
-            print("Perhaps an alternative unitary would work?\n")
+            else:
+                print("oops, you built the state:")
+                print(state)
+                print("Perhaps an alternative unitary would work?\n")
 
 def test_minus_input_to_one(prog):
 
     wf_sim = WavefunctionSimulator()
 
     with local_qvm():
-        
-        state = wf_sim.wavefunction(prog)
-        amp = 1
+        if len(prog._instructions) == 0:
+            print("Hmm, I'm not sure you have any gates applied to the state. Better add some..") 
+        else:    
+            state = wf_sim.wavefunction(prog)
+            amp = 1
 
-        if abs(state[0].real - amp) < 0.0001:
-            print("\nExcellent! You built the state:")
-            print(state)
-        elif abs(state[1].real - amp) < 0.0001 and len(prog._instructions) < 4:
-            print("\nAh shucks, you made:")
-            print(state)
-            print("Maybe you need more gates?")
-        else:
-            print("oops, you built the state:")
-            print(state)
-            print("Perhaps an alternative unitary would work?\n")
+            if abs(state[0].real - amp) < 0.0001:
+                print("\nExcellent! You built the state:")
+                print(state)
+            elif abs(state[1].real - amp) < 0.0001 and len(prog._instructions) < 4:
+                print("\nAh shucks, you made:")
+                print(state)
+                print("Maybe you need more gates?")
+            else:
+                print("oops, you built the state:")
+                print(state)
+                print("Perhaps an alternative unitary would work?\n")
