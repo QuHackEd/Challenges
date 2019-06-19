1bf2ddbce6e36a887585df6974d9f74e88ba2b81
BrianCoyle <s1671233@sms.ed.ac.uk>
2019-06-19 12:39:54 +0100

    Measurement Challenge Finished

--- tests/test_challenge_measurement.py
+++ tests/test_challenge_measurement.py
@@ -33,4 +33,57 @@ def test_measurement_run_results(results, num_shots):
 
                 if num_shots < 50:
                      print('Maybe try a bigger number of  measurement shots..')
+
+    return
+                
+def test_measurement_runandmeasure_results(results, num_shots):
+    
+    if not isinstance(results, dict):
+        print('It doesn\'t look like the results are in the correct format.. did you use run_and_measure?')
+        
+    else:
+        if not list(results.keys()) == [0, 1, 2, 7]:
+            print('It doesn\'t seem like the right qubits have been measured? Did you use the correct chip?')
+        else:
+            if not results[0].all() == 1:
+                print('Hmm, it doesn\'t look like the outcome from qubit 0 was always 1. Maybe try adding some gates?')
+            else:
+                     
+                if not results[7].all() == 1:
+                    print('Hmm, it doesn\'t look like the outcome from qubit 7 was always 1. Maybe try adding some gates?')  
+                else: 
+                    print('Good job!')
+           
+    return 
+                      
+                      
+                      
+                      
+def test_measurement_challenge(results_list, num_shots):
+    
+    if not isinstance(results_list, list):
+        print('*results_list* is not in a list format.')
+    else:
+        try:
+            summed_results = sum(results_list)
+        except:
+            print('Something went wrong')
+        if abs(summed_results - 3*num_shots/4) < 0.1*num_shots:
+                print("Those statistics for look right, well done!")
+
+        else:
+            print("oops, those statistics don't look right... It could just be that you got a disproportionately high number of one outcome or another though. Try a couple more times.")
+
+            if num_shots < 50:
+                print('Maybe try a bigger number of measurement shots..') 
+    return
+                      
+                      
+                      
+                      
+                      
+                      
+                      
+                      
+            
                  
