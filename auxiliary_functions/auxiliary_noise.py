b2cefe1dbed1a69fe2d9226769d411783263b8ef
BrianCoyle <s1671233@sms.ed.ac.uk>
2019-07-12 16:42:50 +0100

    RB continued

--- auxiliary_functions/auxiliary_noise.py
+++ auxiliary_functions/auxiliary_noise.py
@@ -4,33 +4,50 @@ from pyquil.gates import *
 import os, inspect, sys
 import numpy as np
 from pyquil.noise import append_kraus_to_gate
+from pyquil.quilbase import DefGate
+from pyquil.parameters import Parameter
 
-# Define function to return the operators sqrt(p)I, sqrt(1-p)Z
-def dephasing_kraus_map(p):
-
-    return [np.sqrt(1-p)*np.eye(2), np.sqrt(p)*np.diag([1, -1])]
 
 # Define the sequence of noisy clifford operations here.
 
-def define_noisy_cliffords(p):
+def define_noisy_cliffords(circuit, p):
+    p = Parameter('p')
     noisy_cliffords = []
-    corrupted_X = append_kraus_to_gate(dephasing_kraus_map(p), np.array([[0, 1], [1, 0]]))
-    corrupted_Z = append_kraus_to_gate(dephasing_kraus_map(p), np.array([[1, 0], [0, -1]]))
-    corrupted_H = append_kraus_to_gate(dephasing_kraus_map(p), 1/np.sqrt(2)*np.array([[1, 1], [1, -1]]))
+    noisy_cliffords_defn = []
+    corrupted_X = np.array([[0, 1-p], [1-p, 0]])
+    corrupted_Z = np.array([[1, 0], [0, -1]])
+    corrupted_H = (1/np.sqrt(2))*np.array([[1, 1-2*p], [1-2*p, 1]])
 
-    noisy_cliffords.append(corrupted_X)
-    noisy_cliffords.append(corrupted_Z)
-    noisy_cliffords.append(corrupted_H)
-
-    return noisy_cliffords
-
-def override_cliffords_in_circuit(circuit, noisy_cliffords):
-    # Override noiseless clifford gates in circuit with dephased versions
-    # when they are applied
+    gate_definition_X = DefGate('NOISY_X', corrupted_X, [p])
+    NOISY_X = gate_definition_X.get_constructor()
     
-    [corrupted_X, corrupted_Z, corrupted_H] = noisy_cliffords
-    noisy_x =  circuit.define_noisy_gate("X", [0], corrupted_X)
-    noisy_z =  circuit.define_noisy_gate("Z", [0], corrupted_Z)
-    noisy_h =  circuit.define_noisy_gate("H", [0], corrupted_H)
+    gate_definition_Z = DefGate('NOISY_Z', corrupted_Z)
+    NOISY_Z = gate_definition_Z.get_constructor() 
+    
+    gate_definition_H = DefGate('NOISY_H', corrupted_H, [p])
+    NOISY_H = gate_definition_H.get_constructor()
 
-    return circuit
+    noisy_cliffords.append(NOISY_X)
+    noisy_cliffords.append(NOISY_Z)
+    noisy_cliffords.append(NOISY_H)
+    
+#     noisy_cliffords_defn.append(gate_definition_X)
+#     noisy_cliffords_defn.append(gate_definition_Z)
+#     noisy_cliffords_defn.append(gate_definition_H)
+    
+    circuit += gate_definition_X
+    circuit += gate_definition_Z
+    circuit += gate_definition_H
+    
+    return circuit, noisy_cliffords
+
+# def define_noisy_gates_in_circuit(circuit, noisy_cliffords_defn):
+#     # Override noiseless clifford gates in circuit with dephased versions
+#     # when they are applied
+    
+#     [gate_definition_X, gate_definition_Z, gate_definition_H] = noisy_cliffords_defn
+#     circuit += gate_definition_X
+#     circuit += gate_definition_Z
+#     circuit += gate_definition_H
+  
+#     return circuit
